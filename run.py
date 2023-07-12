31b47837cc13d83f80f2a5b88d33ec00fd106d5f
KseniaSOS <sossin.ksenia@gmail.com>
2023-07-12 21:34:00 +0200

    Update run script with first draft based on core game functions.

--- run.py
+++ run.py
@@ -2,18 +2,34 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-import json
+from helper_functions import parse_layout, create_card_frame, create_empty_frame
+from classes import BoardElement, Board
+from game import run_game
 
-
-def run_game():
-    """
-    Entry point with logo and welcome message. User should enter his name.(try/except)
-    """
-    print("welcome")
-    print("welcome_text")
-    input("\nShall we get started? Hit Enter to continue\n")
-    user_name = input("Please enter your name:\n")
-
+LAYOUT_JSON = "layout.json"
 
 if __name__ == '__main__':
-    run_game()
+    # parse the layout
+    layout = parse_layout(LAYOUT_JSON)
+
+    # create a list of available card elements
+    card_elements = []
+    card_names = layout['card_elements'].keys()
+    for card_name in card_names:
+        element = BoardElement(card_name, layout['card_elements'][card_name], layout['frame_elements'],
+                               layout['frame_size'])
+        card_elements.append(element)
+
+    # create empty board element
+    empty_frame = create_empty_frame(layout['frame_size'])
+    empty_frame_element = BoardElement(" ",empty_frame, layout['frame_elements'], layout['frame_size'])
+
+    # initialize the game board
+    my_board = Board(layout['board_size'], empty_frame_element)
+    my_board.populate_board(card_elements)
+    my_board.flip_all_elements()
+    print(my_board)
+    exit(0)
+
+    # run the game
+    run_game(my_board, layout['messages'])
