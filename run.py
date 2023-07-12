da41ceea6dee5313018a75874ab99a78d154e5b7
KseniaSOS <sossin.ksenia@gmail.com>
2023-07-12 21:22:25 +0200

    First draft of main function.

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import json
+
+
+def run_game():
+    """
+    Entry point with logo and welcome message. User should enter his name.(try/except)
+    """
+    print("welcome")
+    print("welcome_text")
+    input("\nShall we get started? Hit Enter to continue\n")
+    user_name = input("Please enter your name:\n")
+
+
+if __name__ == '__main__':
+    run_game()
