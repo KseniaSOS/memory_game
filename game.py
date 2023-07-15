93bb73cd9506d1469f6053fd9f0e311d4e91f54a
KseniaSOS <sossin.ksenia@gmail.com>
2023-07-15 20:57:35 +0200

    Add typing library, break too long lines into multiple lines where is needed. Add os system, where the terminal should be cleared

--- game.py
+++ game.py
@@ -1,21 +1,22 @@
+import os
 from random import randint
 from time import sleep
+from typing import List
 
-
-from helper_functions import create_nested_list, check_input
-from classes import Board
+from helper_functions import create_nested_list, check_input, check_user_name
+from classes import BoardElement, Board
 
 
 def display_rules():
     """
-    This function displays rules in terminal
+    This function displays rules in terminal.
     """
     rules = """
     The objective of Memory Game is to defeat your computer opponent by
-    correctly guessing a pair of cards. At the beggining of the game, all
-    16 cards with ASCCI images will be randomply placed in the terminal.
+    correctly guessing a pair of cards. At the beginning of the game, all
+    16 cards with ASCII images will be randomly placed in the terminal.
     At first You will need to guess the first card, then the second one.
-    If they match, You will get a point. Iterate untill all pair of cards
+    If they match, You will get a point. Iterate until all pairs of cards
     are found."""
 
     return rules
@@ -23,30 +24,30 @@ def display_rules():
 
 def display_score(player_score, computer_score):
     """
-    This functions displays players and computer score.
+    This function displays players and computer score.
     """
     return print(
         f"Current score: player - {player_score}, computer - {computer_score}")
 
 
-def player_turn(index_dict, messages: dict):
+def player_turn(index_dict: dict, messages: dict):
     """
     Asks the user for two input indices.
     """
     # pre-allocate some variables
     selected_indexes = []
     selected_indexes_str = []
-    _index_dict = index_dict.copy()
 
+    prev_val = ' '
     for i in range(2):
         # check user input
-        keys = list(_index_dict.keys())
-        input_value = check_input(
-            keys, messages['input_message'], messages['error_message'])
-        selected_index = _index_dict[input_value]
+        keys = list(index_dict.keys())
+
+        input_value = check_input(keys, prev_val, i, messages)
+        prev_val = input_value
+        selected_index = index_dict[input_value]
         selected_indexes.append(selected_index)
         selected_indexes_str.append(input_value)
-        _index_dict.pop(input_value)
 
     return selected_indexes, selected_indexes_str
 
@@ -64,22 +65,29 @@ def computer_turn(index_list):
         idx = randint(0, len(_index_list) - 1)
         selected_index = _index_list[idx]
         selected_indexes.append(selected_index)
-        selected_indexes_str.append(str(selected_index[0]) + str(selected_index[1]))
+        selected_indexes_str.append(
+            str(selected_index[0]) + str(selected_index[1]))
         _index_list.pop(idx)
 
     return selected_indexes, selected_indexes_str
 
 
-def run_game(board: Board, layout: dict):
+def run_game(board: Board, layout: dict, card_elements: List[BoardElement]):
     """
-    The functions starts a game. Prints logo and welcome messages.
+    The function starts a game. Prints logo and welcome messages.
     """
     print(layout["logo"])
     input("\nShall we get started? Hit Enter to continue...\n")
-    user_name = input("Please enter your name:\n")
+
+    # here we need the check for non-empty a non-numeric string (new function)
+    user_name = check_user_name(layout['messages']['error_messages'])
+
     print(f"Welcome {user_name}! Here are the rules:")
     print(display_rules())
     input("Hit Enter to continue...\n")
+
+    # populate the board from the card element list
+    board.populate_board(card_elements)
 
     # initialize scores
     player_score = 0
@@ -89,15 +97,16 @@ def run_game(board: Board, layout: dict):
     nb_flipped_cards = board.count_flipped_cards()
     nb_used_board_cells = board.nb_used_board_cells
 
-    # create nested list of indexes packed in a dictionary 
+    # creates nested list of indexes packed in a dictionary
     # for user input checking and element selection
     rows = list(range(board.board_size[0]))
     cols = list(range(board.board_size[1]))
     # list of board indexes
     nested_list = create_nested_list(rows, cols)
-    # string versions of the board indexes 
+    # string versions of the board indexes
     # (easier to use for input checking, as it is string)
-    keys = [str(nested_list[x][0]) + str(nested_list[x][1]) for x in range(len(nested_list))]
+    keys = [str(nested_list[x][0]) + str(nested_list[x][1])
+            for x in range(len(nested_list))]
     index_dict = dict(zip(keys, nested_list))
 
     # launch the game
@@ -106,7 +115,8 @@ def run_game(board: Board, layout: dict):
         print(board)
 
         # player takes a turn
-        selected_indexes, selected_indexes_str = player_turn(index_dict, layout['messages'])
+        selected_indexes, selected_indexes_str = player_turn(
+            index_dict, layout['messages'])
         for i in range(2):
             board.flip_board_element(selected_indexes[i])
         print(board)
@@ -114,7 +124,8 @@ def run_game(board: Board, layout: dict):
         # check if successful and update the score.
         # Prints an appropriate message.
         # Creates a delay before executing next code(sleep).
-        if board.check_board_element_match(selected_indexes[0], selected_indexes[1]):
+        if board.check_board_element_match(
+                selected_indexes[0], selected_indexes[1]):
             player_score += 1
             print("Player successful!")
             display_score(player_score, computer_score)
@@ -127,6 +138,7 @@ def run_game(board: Board, layout: dict):
             for i in range(2):
                 board.flip_board_element(selected_indexes[i])
         sleep(5)
+        os.system('cls')
 
         # check if all cards are already flipped by the player
         if nb_flipped_cards == nb_used_board_cells:
@@ -140,7 +152,8 @@ def run_game(board: Board, layout: dict):
         print(board)
 
         # check if successful and update the score
-        if board.check_board_element_match(selected_indexes[0], selected_indexes[1]):
+        if board.check_board_element_match(
+                selected_indexes[0], selected_indexes[1]):
             computer_score += 1
             print("Computer successful!")
             display_score(player_score, computer_score)
@@ -153,6 +166,7 @@ def run_game(board: Board, layout: dict):
             for i in range(2):
                 board.flip_board_element(selected_indexes[i])
         sleep(5)
+        os.system('cls')
 
     # finish the game and print result
     if player_score > computer_score:
