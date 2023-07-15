6ad7ee395b37a403001b4636f5ac67ef02d8796a
KseniaSOS <sossin.ksenia@gmail.com>
2023-07-15 20:41:13 +0200

    Break too long line into multiple lines where is needed. Add restart. Add os library.

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+
 from helper_functions import parse_layout, create_empty_frame
 from classes import BoardElement, Board
 from game import run_game
@@ -12,17 +14,23 @@ if __name__ == '__main__':
     card_elements = []
     card_names = layout['card_elements'].keys()
     for card_name in card_names:
-        element = BoardElement(card_name, layout['card_elements'][card_name], layout['frame_elements'],
-                               layout['frame_size'])
+        element = BoardElement(card_name, layout['card_elements'][
+            card_name], layout['frame_elements'], layout['frame_size'])
         card_elements.append(element)
 
     # create empty board element
     empty_frame = create_empty_frame(layout['frame_size'])
-    empty_frame_element = BoardElement(" ", empty_frame, layout['frame_elements'], layout['frame_size'])
+    empty_frame_element = BoardElement(" ", empty_frame, layout[
+        'frame_elements'], layout['frame_size'])
 
     # initialize the game board
     my_board = Board(layout['board_size'], empty_frame_element)
-    my_board.populate_board(card_elements)
 
     # run the game
-    run_game(my_board, layout)
+    restart = ""
+    while restart == "":
+        os.system('cls')
+        run_game(my_board, layout, card_elements)
+        restart = input("Press ENTER to restart:\n")
+        while restart != "":
+            restart = input("Press ENTER to restart:\n")
